98f20f4781aeb2320a2d4d5baa65810f1f8d4638
bobscoe <mjg5kc@mail.missouri.edu>
2018-12-25 14:37:10 -0600

    added final layout; logos,links, styles; add git ignore

--- src/app/splash/splash.controller.js
+++ src/app/splash/splash.controller.js
@@ -5,7 +5,12 @@
 
   function Splash() {
     var vm = this;
-    vm.msg = "MERRY CHRIHMUS!";
+    vm.msg = "Site Coming Soon!";
+    vm.tableData = [
+      {id:'apple_music', url:'https://itunes.apple.com/us/artist/reggie-pearl/1441847594', icon:'am', verb:"Stream"},
+      {id:'spotify', url:'https://open.spotify.com/artist/0bqF0eDCWNkie7lHRS9fDe?si=yPEOEGiYSMi0hr_Uh9AUjw', icon:'s', verb:"Stream"},
+      {id:'soundcloud', url:'https://m.soundcloud.com/reggie-pearl', icon:'sc', verb:"Stream"},
+    ]
     activate();
     //------------------//
     function activate() {}
